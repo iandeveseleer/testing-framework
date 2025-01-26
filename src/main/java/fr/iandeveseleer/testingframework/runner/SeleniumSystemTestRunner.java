7aa2504000736cb16e711fa6ddd03c246ad30169
Ian DEVESELEER <contact@iandeveseleer.fr>
2025-01-26 10:01:17 +0100

    Update SeleniumSystemTestRunner : extends SystemTestRunner and remove call to static methods in before and after suite

--- src/main/java/fr/iandeveseleer/testingframework/runner/SeleniumSystemTestRunner.java
+++ src/main/java/fr/iandeveseleer/testingframework/runner/SeleniumSystemTestRunner.java
@@ -3,21 +3,17 @@ package fr.iandeveseleer.testingframework.runner;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.platform.suite.api.AfterSuite;
 import org.junit.platform.suite.api.BeforeSuite;
-import org.junit.platform.suite.api.IncludeClassNamePatterns;
 
 @Slf4j
-@IncludeClassNamePatterns(".*SystemTest$")
-public abstract class SeleniumSystemTestRunner {
+public abstract class SeleniumSystemTestRunner extends SystemTestRunner {
 
     @BeforeSuite
     static void beforeSuite() {
-        SystemTestRunner.beforeSuite();
         LOGGER.info("Before Selenium suite");
     }
 
     @AfterSuite
     static void afterSuite() {
-        SystemTestRunner.afterSuite();
         LOGGER.info("After Selenium suite");
     }
 }
